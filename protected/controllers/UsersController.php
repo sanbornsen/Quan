9c391e4d2d3e66bccd84f6d8fd9e086c2bb9471a
sanborn <sanborn@sanborn-SVE14112ENB.(none)>
2013-06-12 13:31:59 +0530

    New user controller added

--- protected/controllers/UsersController.php
+++ protected/controllers/UsersController.php
@@ -28,12 +28,16 @@ class UsersController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+				'actions'=>array('index','view','views'),
 				'users'=>array('admin'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','login'),
 				'users'=>array('*'),
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('views','view','update','updatepart'),
+				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete','update'),
@@ -51,12 +55,42 @@ class UsersController extends Controller
 	 */
 	public function actionView($id)
 	{
-	$id = (($id/10000000000)-342)/77;
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
+		$model = $this->loadModel($id);
+		$dataProvider = new CActiveDataProvider('Question',array('criteria'=>array('condition'=>'user_id LIKE "'.$model->username.'"','order'=>'q_id DESC'),));
+		if($model->username == Yii::app()->user->getId() && ($model->image == "" || $model->education == "" || $model->job == "")){
+			$this->render('update',array(
+			'model'=>$model,
+			));
+		}
+		else{
+			$this->render('view',array(
+				'model'=>$model,
+				'dataProvider'=>$dataProvider,
+			));
+		}
 	}
-
+	
+	/**
+	 * Another action for views users
+	 */
+	public function actionViews($id)
+	{
+		$user = Users::model()->find("username LIKE '".$id."'");
+		$id = $user->user_id;
+		$model = $this->loadModel($id);
+		if($model->username == Yii::app()->user->getId() && ($model->image == "" || $model->education == "" || $model->job == "")){
+			$this->render('update',array(
+			'model'=>$model,
+			));
+		}
+		else{
+			$dataProvider = new CActiveDataProvider('Question',array('criteria'=>array('condition'=>'user_id LIKE "'.$model->username.'"','order'=>'q_id DESC'),));
+			$this->render('view',array(
+				'model'=>$model,
+				'dataProvider'=>$dataProvider,
+			));
+		}
+	}
 	/**
 	 * Creates a new model.
 	 * If creation is successful, the browser will be redirected to the 'view' page.
@@ -95,12 +129,134 @@ class UsersController extends Controller
 		// Uncomment the following line if AJAX validation is needed
 		 $this->performAjaxValidation($model);
 
-		if(isset($_POST['Users']))
+		if(isset($_POST))
 		{
+			die(var_dump($_POST));
 			$model->attributes=$_POST['Users'];
 			$model->password = md5($_POST['Users']['password']);
 			if($model->save())
 				$this->redirect(array('view','id'=>$model->user_id));
+		}
+
+		$this->render('update',array(
+			'model'=>$model,
+		));
+	}
+	
+	
+	/**
+	 * Updates a particular model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id the ID of the model to be updated
+	 */
+	public function actionUpdatepart($id)
+	{
+		Yii::import("image.EUploadedImage", true);	
+		$model=$this->loadModel($id);
+
+		// Uncomment the following line if AJAX validation is needed
+		 $this->performAjaxValidation($model);
+		if(isset($_POST['Users']))
+		{
+			
+			if(isset($_POST['Users']['college'])){       //save education details
+				$college_size = sizeof($_POST['Users']['college']);
+				$college_name = $_POST['Users']['college'];
+				$course_name = $_POST['Users']['course'];
+				$course_from = $_POST['Users']['course_from'];
+				$course_to = $_POST['Users']['course_to'];
+				$edu = array();
+				
+				for($i=0;$i<$college_size;$i++){
+					if($college_name[$i] != ""){
+						$edu["education".($i+1)] = $college_name[$i]."|".$course_name[$i]."|".$course_from[$i]."|".$course_to[$i];
+						$find_college = Institution::model()->find("inst_name LIKE '".trim($college_name[$i])."'");
+						if($find_college){
+							$student = explode(",",$find_college->inst_student);
+							if(!in_array($id, $student))
+								array_push($student, $id);
+							//print_r($student);
+							$find_college->inst_student = implode(",",$student);
+							$student = explode(",",$find_college->inst_follower);
+							if(!in_array($id, $student))
+								array_push($student, $id);
+							$find_college->inst_follower = implode(",",$student);
+							$find_college->save(false);
+						}
+						else {
+							$college = new Institution;
+							$college->inst_name = trim($college_name[$i]); 
+							$college->inst_student = $id;
+							$college->inst_follower=$id;
+							$college->inst_creator=$id;
+							$college->save(false);
+						}
+							
+					}
+				}
+				
+				$model->education = json_encode($edu);
+			}
+			
+			if(isset($_POST['Users']['company'])){			//save job details
+				$job_size = sizeof($_POST['Users']['company']);
+				$company_name = $_POST['Users']['company'];
+				$post = $_POST['Users']['post'];
+				$job_from = $_POST['Users']['job_from'];
+				$job_to = $_POST['Users']['job_to'];
+				$job = array();
+				
+				for($i=0;$i<$college_size;$i++){
+					if($company_name[$i]!=""){
+						$job["job".($i+1)] = $company_name[$i]."|".$post[$i]."|".$job_from[$i]."|".$job_to[$i];
+					$find_com = Company::model()->find("com_name LIKE '".trim($company_name[$i])."'");
+						if($find_com){
+							$employe = explode(",",$find_com->com_emp);
+							if(!in_array($id, $employe))
+								array_push($employe, $id);
+							//print_r($employe);
+							$find_com->com_emp = implode(",",$employe);
+							$employe = explode(",",$find_com->com_follower);
+							if(!in_array($id, $employe))
+								array_push($employe, $id);
+							$find_com->com_follower = implode(",",$employe);
+							$find_com->save(false);
+						}
+						else {
+							$com = new Company;
+							$com->com_name = trim($company_name[$i]); 
+							$com->com_emp = $id;
+							$com->com_follower=$id;
+							$com->com_creator=$id;
+							$com->save(false);
+						}
+					}
+				}
+				
+				$model->job = json_encode($job);
+			}
+			
+			$model->attributes=$_POST['Users'];
+			
+			if($model->image == "unknown.jpg"){
+				$uploadedimage=EUploadedImage::getInstance($model,'image');
+				$uploadedimage->maxWidth = 300;
+				$uploadedimage->maxHeight = 300;
+				$ext = end(explode(".",$uploadedimage));
+				$filename = $model->username.".".$ext;
+				$model->image = $filename;
+				$url = "../".Yii::app()->baseUrl."/images/users/".$filename;
+				$uploadedimage->thumb = array(
+				    'maxWidth' => 100,
+				    'maxHeight' => 100,
+				    'dir' => 'thumbs',
+    				    'prefix' => 'thumb_',
+				);
+				$uploadedimage->saveAs($url);
+			}			
+			if($model->save(false)){
+				$this->redirect(array('view','id'=>$model->user_id));
+			}
 		}
 
 		$this->render('update',array(
